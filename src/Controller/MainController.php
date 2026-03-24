b75824b5083ff0e895a5fe61f6b95e5d25ada35a
Beïza COSGUN <bcosgun781@gmail.com>
2026-03-24 15:11:26 +0100

    Installing PHP CS Fixer

---
+++ src/Controller/MainController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Tests\Functional\Bundle\TestBundle\TestServiceContainer\ResettableService;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function HomePage() : Response
+    {
+        return new Response("<strong>Hello World !</strong>");
+    }
+
+}
