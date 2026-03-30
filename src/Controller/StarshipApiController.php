1bcef4d59d1fadb576c4a35063a362b58d669de7
Beïza COSGUN <bcosgun781@gmail.com>
2026-03-30 19:22:41 +0200

    Fin C11 : Itinéraires plus sophistiqués : conditions

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,21 +2,34 @@
 
 namespace App\Controller;
 
+use App\Model\Starship;
 use App\Repository\StarshipRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Route('/api/starships')]
 class StarshipApiController extends AbstractController
 {
-    #[Route('/api/starships')]
+    #[Route('/api/starships', methods: ['GET'])]
     public function getCollection(StarshipRepository $repository) : Response
     {
         $starships = $repository->findAll();
 
         return $this->json($starships);
     }
+
+    #[Route('/{id<\d+>}', methods: ['GET'])]
+    public function get (int $id, StarshipRepository $repository) : Response
+    {
+        $starship = $repository->find($id);
+        if (!$starship) {
+            throw $this->createNotFoundException('Vaisseau non trouvé');
+        }
+        return $this->json($starship);
+    }
+
 
 
 }
