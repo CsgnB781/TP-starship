09e1f21e2778669f97a2cb3e45e859beba14b16a
Beïza COSGUN <bcosgun781@gmail.com>
2026-03-30 18:45:43 +0200

    Fin C10 : Créez votre propre service

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\StarshipRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,29 +11,10 @@ use Symfony\Component\HttpFoundation\Response;
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(LoggerInterface $logger) : Response
+    public function getCollection(StarshipRepository $repository) : Response
     {
-        $logger->info('collection de vaisseaux spatiaux récupérée');
-        $starships = [
-            [
-                'name' => 'USS LeafyCruiser (NCC-0001)',
-                'class' => 'Garden',
-                'captain' => 'Jean-Luc Pickles',
-                'status' => 'taken over by Q',
-            ],
-            [
-                'name' => 'USS Espresso (NCC-1234-C)',
-                'class' => 'Latte',
-                'captain' => 'James T. Quick!',
-                'status' => 'repaired',
-            ],
-            [
-                'name' => 'USS Wanderlust (NCC-2024-W)',
-                'class' => 'Delta Tourist',
-                'captain' => 'Kathryn Journeyway',
-                'status' => 'under construction',
-            ],
-        ];
+        $starships = $repository->findAll();
+
         return $this->json($starships);
     }
 
