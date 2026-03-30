aa254df5deca9b605a15ba83f8b05959490bfcaa
Beïza COSGUN <bcosgun781@gmail.com>
2026-03-30 16:39:54 +0200

    Premier commit : fin du C05

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Tests\Functional\Bundle\TestBundle\TestServiceContainer\ResettableService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
-    public function HomePage() : Response
+    public function HomePage(): Response
     {
-        return new Response("<strong>Hello World !</strong>");
+        $starshipCount = 457;
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+        return $this->render("main/homepage.html.twig", [
+            'numberOfStarships' => $starshipCount,
+            'myShip' => $myShip,
+        ]);
     }
-
 }
